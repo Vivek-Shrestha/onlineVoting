85760c5cbe681f86428ef731588822ee6a963502
bibek <sthabibek789@gmail.com>
2022-05-29 20:21:09 +0545

    comit

---
+++ layouts/footer.php
@@ -0,0 +1,33 @@
+<!-- </div>
+</div>
+  <!--   Core JS Files   -->
+  <script src="./assets/js/core/jquery.min.js"></script>
+  <script src="./assets/js/core/popper.min.js"></script>
+  <script src="./assets/js/core/bootstrap.min.js"></script>
+  <script src="./assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
+  <!--  Google Maps Plugin    -->
+  <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+  <!-- Chart JS -->
+  <script src="./assets/js/plugins/chartjs.min.js"></script>
+  <!--  Notifications Plugin    -->
+  <script src="./assets/js/plugins/bootstrap-notify.js"></script>
+  <!-- Control Center for Now Ui Dashboard: parallax effects, scripts for the example pages etc -->
+  <script src="./assets/js/now-ui-dashboard.min.js?v=1.5.0" type="text/javascript"></script><!-- Now Ui Dashboard DEMO methods, don't include it in your project! -->
+  <link src="fontawesome-4.7.0/fonts/"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <script src="./assets/demo/demo.js"></script>
+<div class="container-fluid content-mt1">
+ 
+  <p class="text-center footerIcon">
+    <span><i class="fa fa-Facebook"><a href="#">Facebook</a></i></span>
+  <span><i class="fa fa-Instagram"><a href="#">Instagram</a></i></span></p>
+  <p class="text-center">&copy;2021bibek</p>    
+  </div>
+  <script>
+    $(document).ready(function() {
+      // Javascript method's body can be found in assets/js/demos.js
+      demo.initDashboardPageCharts();
+
+    });
+  </script>
+</body>
+</html> 
---
+++ layouts/sidebar.php
@@ -0,0 +1,44 @@
+<div class="wrapper ">
+    <div class="sidebar" data-color="orange">
+      <!--
+        Tip 1: You can change the color of the sidebar using: data-color="blue | green | orange | red | yellow"
+    -->
+      <div class="logo">
+       
+        </a>
+        <a href="http://www.creative-tim.com" class="simple-text logo-normal">
+     Student Record
+        </a>
+      </div>
+      <div class="sidebar-wrapper" id="sidebar-wrapper">
+        <ul class="nav">
+          <li class="active ">
+            <a href="./dashboard.html">
+              <i class="now-ui-icons design_app"></i>
+              <p>Dashboard</p>
+            </a>
+          </li>
+          <!-- <li>
+            <a href="./student_edit.php">
+              <i class="now-ui-icons education_atom"></i>
+              <p>EDIT</p>
+            </a>
+          </li> -->
+          <li>
+            <a href="./student_display.php">
+              <i class="now-ui-icons location_map-big"></i>
+              <p>DISPLAY</p>
+            </a>
+          </li>
+
+          <li>
+            <a href="./form.php">
+              <i class="now-ui-icons location_map-big"></i>
+              <p>Admin page</p>
+            </a>
+          </li>
+
+         
+        </ul>
+      </div>
+    </div>
