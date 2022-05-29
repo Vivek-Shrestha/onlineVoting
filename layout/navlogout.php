5e674f80ae76bfc640a004ea2f08e9940db1019b
bibek <sthabibek789@gmail.com>
2022-05-29 20:20:53 +0545

    comit

---
+++ layout/navlogout.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title> Voting </title>
+
+  <!-- Bootstrap -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+
+  <link href='http://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed' rel='stylesheet' type='text/css'>
+
+  <style>
+    body {
+      margin: 0px;
+      padding: 0px;
+    }
+
+    .headerFont {
+      font-family: 'Ubuntu', sans-serif;
+      font-size: 24px;
+    }
+
+    .subFont {
+      font-family: 'Raleway', sans-serif;
+      font-size: 14px;
+
+    }
+
+    .specialHead {
+      font-family: 'Oswald', sans-serif;
+    }
+
+    .normalFont {
+      font-family: 'Roboto Condensed', sans-serif;
+    }
+
+    a {
+      color: #FFFFFF;
+      text-decoration: none;
+    }
+
+    a:link {
+      color: #FFFFFF;
+      text-decoration: none;
+    }
+
+    /* visited link */
+    a:visited {
+      color: #FFFFFF;
+      text-decoration: none;
+    }
+
+    /* mouse over link */
+    a:hover {
+      color: #FFFFFF;
+      text-decoration: none;
+    }
+
+    /* selected link */
+    a:active {
+      color: #FFFFFF;
+      text-decoration: none;
+    }
+  </style>
+
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+  <div class="container">
+    <nav class="navbar navbar-default navbar-fixed-top navbar-inverse
+    " role="navigation">
+      <div class="container">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#example-nav-collapse">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <div class="navbar-header">
+
+          <a href="index.php" class="navbar-brand headerFont text-lg"><strong>eVoting</strong></a>
+        </div>
+
+        <div class="collapse navbar-collapse" id="example-nav-collapse">
+          <ul class="nav navbar-nav">
+
+            <li><a href="./adminpanal.php"><span class="subFont"><strong>Admin Panel</strong></span></a></li>
+            <li><a href="./canidate_display.php"><span class="subFont"><strong>Candidate </strong></span></a></li>
+            <li><a href="./voters_display.php"><span class="subFont"><strong>Voters</strong></span></a></li>
+            <li><a href="./total_vote.php"><span class="subFont"><strong>Total Votes</strong></span></a></li>
+
+          </ul>
+
+
+          <span class="normalFont"><a href="index.php" class="btn btn-danger navbar-right navbar-btn"><strong>log out</strong></a></span>
+        </div>
+
+      </div> <!-- end of container -->
+    </nav>
+  </div>
+</body>
+
+</html>
